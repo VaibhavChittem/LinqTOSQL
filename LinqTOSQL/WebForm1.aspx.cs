37cda48d70da9c7359c370c47ec517f892ad26f3
VaibhavChittem <vaibhavchittem@gmail.com>
2019-05-03 14:37:02 +0530

    Add project files.

---
+++ LinqTOSQL/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqTOSQL
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HREntities hREntities = new HREntities();
+            var emp = from employee in hREntities.Employees
+                      where employee.cCity == "Norton"
+                      select employee;
+            GridView1.DataSource = emp.ToList();
+            GridView1.DataBind();
+        }
+    }
+}
